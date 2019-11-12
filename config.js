78360cfb1c62583d1413db6f4dfdda9ed46f299d
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2019-11-11 19:30:08 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+var GAME = {
+  canvas : {
+    width : 600,
+    height : 300
+  },
+  started : true,
+  level : 1
+};
+
+var SANTA = {
+  initialized : false,
+  collected : [],
+  latest : {
+    x : 0,
+    y : 0
+  }
+};
